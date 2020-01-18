279d1f9002d5234dfaa16e4c8fba561991f72dbd
Edward Iga Kigongo <igakigongo@gmail.com>
2020-01-18 05:09:29 +0100

    add a book to the catalog works

--- js/app.js
+++ js/app.js
@@ -1,29 +1,64 @@
 /**
- * Library Specific Code
+ * Domain Models
  */
-let myLibrary = [];
-
 function Book(author, title, pages, isRead){
+  // Ensure that only a valid book can be created - UI should be able to handle these errors
+  if (!author) throw "Invalid author";
+  if (!title) throw "Invalid author";
+  if (!pages || isNaN(pages) || parseInt(pages) < 1) throw Error("Invalid number of pages");
+
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.isRead = isRead;
+  this.isRead = isRead || false;
 }
 
 Book.prototype.info = function(){
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "already read": "not yet read"}`;
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus()}`;
 };
 
 Book.prototype.readStatus = function(){
-  return this.isRead? "Already read": "Not yet read";
+  return this.isRead? "Read": "Not yet read";
 };
 
-function addBookToLibrary(book){
-  myLibrary.unshift(book);
-}
+/**
+ * Library Specific Code
+ */
+const library  = (function Library(){
+  const books = [];
+
+  function addBookToLibrary(newBookEntry){
+    books.unshift(newBookEntry);
+  }
+
+  function getCatalog(){
+    return books;
+  }
+
+  return {
+    addBookToLibrary,
+    getCatalog
+  };
+})();
 
 /**
- *  DOM Helper functions
+ * Prepopulate the array with some books
+ * Consider this to be something similar to an API call
+ */
+function loadSampleBooks() {
+  return new Promise(function(resolve){
+    [new Book("Edward", "Dracula 1992", 762, true), new Book("Edward", "When the sun sets", 200, false),
+      new Book("Fred", "Freddy Kruggar vs Jason", 1000, true)].forEach(function(b){
+      library.addBookToLibrary(b);
+      resolve("Initialized Library");    
+    });
+  });
+}
+
+loadSampleBooks().then(console.log);
+
+/**
+ *  DOM Manipulation functions
  */
 function createCell(text, isData = true, style = null){
   const cell = isData ? document.createElement("td"): document.createElement("th");
@@ -47,43 +82,103 @@ function createRow(book, id){
   const similar = ["title", "author", "pages"]
     .map(prop => createCell(book[prop]),);
 
-
-  tableRow.append(createCell(id, false), ...similar, createCell(book.readStatus(), false, { "font-weight": "bold" }), 
+  tableRow.append(createCell(id, false), ...similar, 
+    createCell(book.readStatus(), false, { "font-weight": "bold" }), 
     createCell("", true, { "text-align": "right"}));
   
   return tableRow;
 }
 
-/**
- * Prepopulate the array with some books
- */
-(function(){
-  addBookToLibrary(new Book("Edward", "Dracula 1992", 762, true));
-  addBookToLibrary(new Book("Edward", "When the sun sets", 200, false));
-  addBookToLibrary(new Book("Fred", "Freddy vs Kruggar", 1000, true));
-  console.log("Initialized Library", myLibrary);
-})();
+function getHTMLInputElementValueByName(rootElement, id){
+  if (!rootElement)
+    throw Error("Invalid root element or dom node");
 
+  if (typeof id === "undefined" || !id)
+    throw Error("Invalid element name");
+
+  const element = rootElement.querySelector(`#${id}`);
+  if (!(element instanceof HTMLInputElement))
+    throw Error("Element is not an HTMLInputElement");
+  
+  return element.value;
+}
+
+function getBookValuesFromForm(rootElement, elementIds){
+  return elementIds.map(function(id){
+    return getHTMLInputElementValueByName(rootElement, id);
+  });
+}
 
 /**
  * Cache DOM elements
  */
-
 const root = document.getElementById("root");
 const gridContainer = root.querySelector("#grid-container");
 const tableBody =  gridContainer.querySelector("table").querySelector("tbody");
+const addBookButton = root.querySelector("#btn-add-book");
+const addBookSection = root.querySelector("#section-add-book");
+const isReadCheckBox = addBookSection.querySelector("#book-is-read");
 
+/**
+ * Define Event Handlers
+ */
+function addBookToLibraryEventHandler(evt){
+  evt.preventDefault();
+  
+  const elementIds = ["book-author", "book-title", "book-total-pages", "book-is-read"];
+  const [author, title, pages, isRead] = getBookValuesFromForm(addBookSection, elementIds);
+
+  try{
+    const newBook = new Book(author, title, +pages, isRead === "true");
+    library.addBookToLibrary(newBook);
+    resetBookEntryForm(addBookSection);
+    render();
+  }catch(err){
+    alert(err);
+  }
+}
+
+function isReadCheckBoxChangeHandler(evt){
+  evt.preventDefault();
+  const isRead = isReadCheckBox.checked;
+  isReadCheckBox.value = isRead;
+}
+
+function resetBookEntryForm(formElement){
+  console.log(formElement);
+  ["book-author", "book-title", "book-total-pages", "book-is-read"].forEach(function(id){
+    const ele = formElement.querySelector(`#${id}`);
+    if (ele){
+      switch(ele.type){
+      case "text":
+      case "number":
+        ele.value = "";
+        break;
+
+      case "checkbox":
+        ele.checked = false;
+        break;
+      }
+    }
+  });
+}
+
+/**
+ * Attach Event Handlers to DOM Elements
+ */
+addBookButton.addEventListener("click", addBookToLibraryEventHandler);
+isReadCheckBox.addEventListener("change", isReadCheckBoxChangeHandler);
 
 /**
  * Render books onto the view (DOM Manipulation)
  */
-(function render(){
+function render(){
 
-  myLibrary.forEach(function(book, index){
-    const tableRow = createRow(book, index + 1);
-    tableBody.append(tableRow);
-  });
+  tableBody.innerHTML = "";
+  tableBody.append(...library.getCatalog().map(function(book, index){
+    return createRow(book, index + 1);
+  }));
 
-  const btn = document.createElement("button", {});
-  console.log(btn);
-})();
+  // const btn = document.createElement("button", {});
+}
+
