fd1da7846d693c3c91f6fb5c8b1f692ad39a9bd1
Edward Iga Kigongo <igakigongo@gmail.com>
2020-01-20 02:25:55 +0100

    set up simple local storage - change the way the form elements are looked up for cleanup after adding a book

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@
 const ApplicationEvents = {
   BOOK_ADDED: "BookAdded",
   BOOK_REMOVED: "BookRemoved",
-  BOOK_STATUS_CHANGED: "BookStatusChanged",
+  BOOK_STATUS_CHANGED: "BookStatusChanged"
 };
 
 /**
  * Domain Models
  */
-function Book(author, title, pages, isRead){
+function Book(author, title, pages, isRead) {
   // Ensure that only a valid book can be created - UI should be able to handle these errors
   if (!author) throw "Invalid author";
   if (!title) throw "Invalid author";
@@ -22,49 +22,73 @@ function Book(author, title, pages, isRead){
   this.isRead = isRead || false;
 }
 
-Book.prototype.info = function(){
+Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus()}`;
 };
 
-Book.prototype.readStatus = function(){
-  return this.isRead? "Read": "Not yet read";
+Book.prototype.readStatus = function() {
+  return this.isRead ? "Read" : "Not yet read";
 };
 
-Book.prototype.toggleStatus = function(){
+Book.prototype.toggleStatus = function() {
   this.isRead = !this.isRead;
 };
 
 /**
  * Library Specific Code
  */
-const library  = (function Library(){
+const library = (function Library() {
   let books = [];
+  let localStorageKey = "catalog-jubei";
+  const supportsLocalStorage = !!window.localStorage;
 
-  function addBookToLibrary(newBookEntry){
+  /**
+	 * Prepopulate the library with some books
+	 */
+  (function loadSampleBooks() {
+    const sampleBooks = [
+      new Book("Edward", "Dracula 1992", 762, true),
+      new Book("Edward", "When the sun sets", 200, false),
+      new Book("Fred", "Freddy Kruggar vs Jason", 1000, true)
+    ];
+
+    sampleBooks.forEach(function(b) {
+      addBookToLibrary(b);
+    });
+  })();
+
+  function addBookToLibrary(newBookEntry) {
     books = [newBookEntry, ...books];
+    supportsLocalStorage && syncLocalStorage();
   }
 
-  function getCatalog(){
+  function getCatalog() {
     return books;
   }
 
-  function removeBook(id){
-    return new Promise(function(resolve){
-      books = books.filter(function(_, index){
+  function removeBook(id) {
+    return new Promise(function(resolve) {
+      books = books.filter(function(_, index) {
         return index !== id;
       }, []);
+      supportsLocalStorage && syncLocalStorage();
       resolve();
     });
   }
 
-  function toggleBookReadStatus(id){
-    return new Promise(function(resolve){
-      books = books.map(function(book, index){
-        if (index === id){
+  function syncLocalStorage() {
+    window.localStorage.setItem(localStorageKey, JSON.stringify(books));
+  }
+
+  function toggleBookReadStatus(id) {
+    return new Promise(function(resolve) {
+      books = books.map(function(book, index) {
+        if (index === id) {
           book.toggleStatus();
         }
         return book;
       });
+      supportsLocalStorage && syncLocalStorage();
       resolve();
     });
   }
@@ -78,33 +102,13 @@ const library  = (function Library(){
 })();
 
 /**
- * Prepopulate the array with some books
- * Consider this to be something similar to an API call
- */
-function loadSampleBooks() {
-  const sampleBooks = [
-    new Book("Edward", "Dracula 1992", 762, true), 
-    new Book("Edward", "When the sun sets", 200, false),
-    new Book("Fred", "Freddy Kruggar vs Jason", 1000, true)];
-    
-  return new Promise(function(resolve){
-    sampleBooks.forEach(function(b){
-      library.addBook(b);
-      resolve("Initialized Library");    
-    });
-  });
-}
-
-loadSampleBooks();
-
-/**
  *  DOM Manipulation functions
  */
-function createCell(text, isData = true, style = null){
-  const cell = isData ? document.createElement("td"): document.createElement("th");
+function createCell(text, isData = true, style = null) {
+  const cell = isData ? document.createElement("td") : document.createElement("th");
   cell.innerHTML = text;
-  if (style){
-    Object.getOwnPropertyNames(style).forEach(function(prop){
+  if (style) {
+    Object.getOwnPropertyNames(style).forEach(function(prop) {
       cell.style[prop] = style[prop];
     });
   }
@@ -112,15 +116,14 @@ function createCell(text, isData = true, style = null){
   return cell;
 }
 
-function createControlButtons(bookIndex, handlers = null){
+function createControlButtons(bookIndex, handlers = null) {
   const { changeBookStatusHandler, removeBookHandler } = handlers;
   const removeBookButton = document.createElement("button");
   removeBookButton.innerHTML = "Remove";
   removeBookButton.setAttribute("data-id", bookIndex);
   removeBookButton.classList.add("btn", "btn-sm", "btn-danger");
 
-  if (removeBookHandler && (typeof removeBookHandler) === "function")
-  {
+  if (removeBookHandler && typeof removeBookHandler === "function") {
     removeBookButton.addEventListener("click", removeBookHandler);
   }
 
@@ -130,50 +133,47 @@ function createControlButtons(bookIndex, handlers = null){
   changeBookStatusButton.classList.add("btn", "btn-sm", "btn-success");
   changeBookStatusButton.style.marginRight = "0.5rem";
 
-
-  if (changeBookStatusHandler && (typeof changeBookStatusHandler) === "function")
+  if (changeBookStatusHandler && typeof changeBookStatusHandler === "function")
     changeBookStatusButton.addEventListener("click", changeBookStatusHandler);
 
   return [changeBookStatusButton, removeBookButton];
 }
 
-function createRow(book, id, createControlButtonsFn, handlers){
-  if (!(book instanceof Book))
-    throw "Cannot create a row for an object that is not a book";
-    
+function createRow(book, id, createControlButtonsFn, handlers) {
+  if (!(book instanceof Book)) throw "Cannot create a row for an object that is not a book";
+
   const tableRow = document.createElement("tr");
 
-  const similar = ["title", "author", "pages"]
-    .map(prop => createCell(book[prop]),);
+  const similar = ["title", "author", "pages"].map(prop => createCell(book[prop]));
 
   const buttons = createControlButtonsFn(id, handlers);
   const buttonsCell = document.createElement("td");
   buttonsCell.append(...buttons);
   buttonsCell.style.textAlign = "right";
 
-  tableRow.append(createCell(id + 1, false), ...similar, 
-    createCell(book.readStatus(), false, { "font-weight": "bold" }), 
-    buttonsCell);
-  
+  tableRow.append(
+    createCell(id + 1, false),
+    ...similar,
+    createCell(book.readStatus(), false, { "font-weight": "bold" }),
+    buttonsCell
+  );
+
   return tableRow;
 }
 
-function getHTMLInputElementValueByName(rootElement, id){
-  if (!rootElement)
-    throw Error("Invalid root element or dom node");
+function getHTMLInputElementValueByName(rootElement, id) {
+  if (!rootElement) throw Error("Invalid root element or dom node");
 
-  if (typeof id === "undefined" || !id)
-    throw Error("Invalid element name");
+  if (typeof id === "undefined" || !id) throw Error("Invalid element name");
 
   const element = rootElement.querySelector(`#${id}`);
-  if (!(element instanceof HTMLInputElement))
-    throw Error("Element is not an HTMLInputElement");
-  
+  if (!(element instanceof HTMLInputElement)) throw Error("Element is not an HTMLInputElement");
+
   return element.value;
 }
 
-function getBookValuesFromForm(rootElement, elementIds){
-  return elementIds.map(function(id){
+function getBookValuesFromForm(rootElement, elementIds) {
+  return elementIds.map(function(id) {
     return getHTMLInputElementValueByName(rootElement, id);
   });
 }
@@ -183,7 +183,7 @@ function getBookValuesFromForm(rootElement, elementIds){
  */
 const root = document.getElementById("root");
 const gridContainer = root.querySelector("#grid-container");
-const tableBody =  gridContainer.querySelector("table").querySelector("tbody");
+const tableBody = gridContainer.querySelector("table").querySelector("tbody");
 const addBookButton = root.querySelector("#btn-add-book");
 const addBookSection = root.querySelector("#section-add-book");
 const isReadCheckBox = addBookSection.querySelector("#book-is-read");
@@ -191,46 +191,46 @@ const isReadCheckBox = addBookSection.querySelector("#book-is-read");
 /**
  * Define Event Handlers
  */
-function addBookToLibraryEventHandler(evt){
+function addBookToLibraryEventHandler(evt) {
   evt.preventDefault();
   const elementIds = ["book-author", "book-title", "book-total-pages", "book-is-read"];
   const [author, title, pages, isRead] = getBookValuesFromForm(addBookSection, elementIds);
 
-  try{
+  try {
     const newBook = new Book(author, title, +pages, isRead === "true");
     library.addBook(newBook);
     document.dispatchEvent(new Event(ApplicationEvents.BOOK_ADDED));
-  }catch(err){
+  } catch (err) {
     alert(err);
   }
 }
 
-function changeBookStatusEventHandler(evt){
+function changeBookStatusEventHandler(evt) {
   evt.preventDefault();
   const { id } = evt.target.dataset;
   library.toggleBookReadStatus(+id);
   document.dispatchEvent(new Event(ApplicationEvents.BOOK_STATUS_CHANGED));
 }
 
-function isReadCheckBoxChangeHandler(evt){
+function isReadCheckBoxChangeHandler(evt) {
   evt.preventDefault();
   const isRead = isReadCheckBox.checked;
   isReadCheckBox.value = isRead;
 }
 
-function removeBookEventHandler(evt){
+function removeBookEventHandler(evt) {
   evt.preventDefault();
-  const {id} = evt.target.dataset;
-  library.removeBook(+id).then(function(){
+  const { id } = evt.target.dataset;
+  library.removeBook(+id).then(function() {
     document.dispatchEvent(new Event(ApplicationEvents.BOOK_REMOVED));
   });
 }
 
-function resetBookEntryForm(formElement){
-  ["book-author", "book-title", "book-total-pages", "book-is-read"].forEach(function(id){
-    const ele = formElement.querySelector(`#${id}`);
-    if (ele){
-      switch(ele.type){
+function resetBookEntryForm(formElement) {
+  const elements = formElement.querySelectorAll("input");
+  elements.forEach(function(ele) {
+    if (ele) {
+      switch (ele.type) {
       case "text":
       case "number":
         ele.value = "";
@@ -253,16 +253,18 @@ isReadCheckBox.addEventListener("change", isReadCheckBoxChangeHandler);
 /**
  * Render books onto the view (DOM Manipulation)
  */
-function render(){
+function render() {
   tableBody.innerHTML = "";
 
-  tableBody.append(...library.getCatalog().map(function(book, index){
-    const handlers = { 
-      removeBookHandler: removeBookEventHandler, 
-      changeBookStatusHandler: changeBookStatusEventHandler
-    };
-    return createRow(book, index, createControlButtons, handlers);
-  }));
+  tableBody.append(
+    ...library.getCatalog().map(function(book, index) {
+      const handlers = {
+        removeBookHandler: removeBookEventHandler,
+        changeBookStatusHandler: changeBookStatusEventHandler
+      };
+      return createRow(book, index, createControlButtons, handlers);
+    })
+  );
 }
 
 document.addEventListener("DOMContentLoaded", render);
